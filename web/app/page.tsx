0f2923a0a470bcc05d52d25f441d127fbbab847c
Cody Adam <cody.ad4@gmail.com>
2025-11-16 11:39:32 +0200

    fix layout of the landing

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -8,12 +8,11 @@ export default function Home() {
     <div className="flex h-full w-full items-center justify-center p-8">
       <div className="flex flex-col gap-2 items-center justify-center">
         <h1 className="text-2xl font-bold">Disclaimer</h1>
-        <p className="text-sm max-w-xs text-center">
+        <p className="text-sm max-w-xs text-center text-pretty">
           For optimal results, enable{" "}
           <span className="font-bold text-white">AUDIO</span> and{" "}
-          <span className="font-bold text-white">FULLSCREEN MODE</span>.<br />
-          This experience is designed for desktop only.
-          <br />
+          <span className="font-bold text-white">FULLSCREEN MODE</span>. This
+          experience is designed for desktop only.{" "}
           <span className="text-white font-semibold">
             Chrome browser is recommended.
           </span>
